c80737bbafbf498f0cda4069c59fd426bcbc81e1
Michael Krebs <michikrbs@gmail.com>
2023-07-31 21:51:04 +0200

    feat: add background service worker

---
+++ src/service-worker.ts
@@ -0,0 +1,76 @@
+const getIndex = async (
+  sender: chrome.runtime.MessageSender,
+  offset: number
+) => {
+  const tabCount = (await chrome.tabs.query({ currentWindow: true })).length;
+  const idx = sender.tab!.index! + offset;
+  return idx === -1 ? tabCount - 1 : tabCount === idx ? 0 : idx;
+};
+
+const file = "styles.css";
+
+chrome.tabs.query({}).then((tabs) => {
+  for (const tab of tabs) {
+    if (tab.url?.startsWith("chrome") || tab.url === undefined) continue;
+    chrome.scripting.insertCSS({ files: [file], target: { tabId: tab.id! } });
+  }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
+  if (tab.url?.startsWith("chrome") || tab.url === undefined) return;
+  chrome.scripting.insertCSS({ files: [file], target: { tabId: tabId } });
+});
+
+chrome.runtime.onMessage.addListener(async (message, sender) => {
+      console.log(message);
+  switch (message.action) {
+    case "css":
+      if (
+        sender.tab!.url?.startsWith("chrome") ||
+        sender.tab!.url === undefined
+      )
+        break;
+      chrome.scripting.insertCSS({
+        files: [file],
+        target: { tabId: sender.tab!.id! },
+      });
+      break;
+    case "changeTabLeft": {
+      const query = { currentWindow: true, index: await getIndex(sender, -1) };
+      chrome.tabs.update((await chrome.tabs.query(query))[0].id!, {
+        active: true,
+      });
+      break;
+    }
+    case "changeTabRight": {
+      const query = { currentWindow: true, index: await getIndex(sender, 1) };
+      chrome.tabs.update((await chrome.tabs.query(query))[0].id!, {
+        active: true,
+      });
+      break;
+    }
+    case "moveTabLeft":
+      chrome.tabs.move(sender.tab!.id!, { index: await getIndex(sender, -1) });
+      break;
+    case "moveTabRight":
+      chrome.tabs.move(sender.tab!.id!, { index: await getIndex(sender, 1) });
+      break;
+    case "openTabActive":
+      chrome.tabs.create({ active: true, url: message.href });
+      break;
+    case "openTab":
+      chrome.tabs.create({ active: false, url: message.href });
+      break;
+    case "duplicateTab":
+      chrome.tabs.duplicate(
+        (await chrome.tabs.query({ active: true, currentWindow: true }))[0].id!
+      );
+      break;
+    case "newWindow":
+      chrome.windows.create({ url: message.href });
+      break;
+    case "privateWindow":
+      chrome.windows.create({ url: message.href, incognito: true });
+      break;
+  }
+});
